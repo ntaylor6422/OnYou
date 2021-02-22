2c662bd6e5f628104ec141e734d20e790f3c8d65
ntaylor6422 <ntaylor6422@gmail.com>
2021-02-22 15:09:10 +0900

    Initial Commit

--- On You/ContentView.swift
+++ On You/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private func createMatrix(price: String, rows: Int) -> [[Int]] {
+        let toDouble = Double(price)!
+        let rdDown = Int(floor(toDouble)) / 2
+        var arr:[[Int]] = []
+        
+        for _ in 0...rows {
+            var tempArr:[Int] = []
+            for _ in 0...rows {
+                let random = Int.random(in: 0..<rdDown)
+                tempArr.append(random)
+            }
+            arr.append(tempArr)
+        }
+
+        return arr
+    }
+    
+    private func handleClick() -> Int {
+        return 3
+    }
+    
+    private func individualCell(value: Int) -> some View {
+        return Button(action: {
+            print(self.handleClick())
+        }, label: {
+            Text("\(value)")
+        })
+    }
+    
+    private func makeRow(cols: [Int]) -> some View {
+        return HStack {
+            ForEach(cols, id:\.self) { col in
+                individualCell(value: col)
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            ForEach(self.createMatrix(price: "37.99", rows: 5), id:\.self) { row in self.makeRow(cols: row)
+                
+            }
+        }
     }
 }
 
