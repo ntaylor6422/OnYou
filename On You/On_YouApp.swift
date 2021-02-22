e54c67b9c8eb6a03a293d0c4123d638837a3cfb1
ntaylor6422 <ntaylor6422@gmail.com>
2021-02-22 14:30:42 +0900

    Initial Commit

---
+++ On You/On_YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_YouApp.swift
+//  On You
+//
+//  Created by Nicholas Ryan Taylor on 2021/02/22.
+//
+
+import SwiftUI
+
+@main
+struct On_YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
